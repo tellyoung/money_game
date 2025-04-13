213cfd6669d87e05a46ce2c9f375b2359340e645
tellyoung <tellyuty@163.com>
2025-04-13 16:30:56 +0800

    init

--- Trading/bt_future_test.sh
+++ Trading/bt_future_test.sh
@@ -4,12 +4,18 @@ clear
 
 
 freqtrade backtesting \
---config /Users/yutieyang/Documents/yuty/yuty_projects/money_game/Trading/user_data/configs/config_test_futrue_TrendFollowingStrategy.json \
+--config /Users/yutieyang/Documents/yuty/yuty_projects/money_game/Trading/user_data/configs/config_test_futrue_IntradayMomentum.json \
 --userdir /Users/yutieyang/Documents/yuty/yuty_projects/money_game/Trading/user_data \
+--strategy IntradayMomentum \
 --datadir /Users/yutieyang/Documents/yuty/yuty_projects/money_game/Data/binance/Vol_top20_futrue_20250101_20250411 \
 --breakdown week \
---strategy TrendFollowingStrategy \
 --strategy-path /Users/yutieyang/Documents/yuty/yuty_projects/money_game/Trading/user_data/strategies \
 --recursive-strategy-search \
 # --dry-run-wallet 100 \
 
+
+
+# ================= TrendFollowingStrategy ===========================
+# --config /Users/yutieyang/Documents/yuty/yuty_projects/money_game/Trading/user_data/configs/config_test_futrue_TrendFollowingStrategy.json \
+# --userdir /Users/yutieyang/Documents/yuty/yuty_projects/money_game/Trading/user_data \
+# --strategy TrendFollowingStrategy \
